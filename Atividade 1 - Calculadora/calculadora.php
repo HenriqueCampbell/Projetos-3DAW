b4b4d0aeccf701f9d4f769d9dc3a5c509bc4ce87
Henrique Campbell <hctrechau@gmail.com>
2026-02-26 19:04:48 -0300

    Finalizado parte HTML

--- Atividade 1 - Calculadora/calculadora.php
+++ Atividade 1 - Calculadora/calculadora.php
@@ -32,16 +32,24 @@
 <!DOCTYPE html>
 <html>
 <body>
-<h1><?php echo 'Minha Calculadora!';?></h1>
+<h1> Minha Calculadora! </h1>
 
 <form method='POST' action='calculadora.php'>
     a:<input type=text name='a'><br>
-    b:<input type=text name='b'>
-    <input type=submit value='Somar'>
+    b:<input type=text name='b'><br>
+    <!-- <input type=submit name='operacao' value='soma'> -->
+
+    <button type="submit" name="operacao" value="soma">Somar</button>
+    <button type="submit" name="operacao" value="subtracao">Subtrair</button>
+    <button type="submit" name="operacao" value="multiplicacao">Multiplicar</button>
+    <button type="submit" name="operacao" value="divisao">Dividir</button>
+
+</form>
+
     <br><br>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        echo 'Resultado: ' . $soma; 
+        echo 'Resultado: ' . $resultado; 
 }
 ?>
     
