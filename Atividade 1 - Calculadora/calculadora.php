e1f97ec3268a8da377975b675cf6c306a92f5b00
Henrique Campbell <hctrechau@gmail.com>
2026-03-08 16:57:11 -0300

    update raiz

--- Atividade 1 - Calculadora/calculadora.php
+++ Atividade 1 - Calculadora/calculadora.php
@@ -26,9 +26,6 @@
                 break;
             case 'radiciacao':
                 $resultado = pow($a, 1 / $b);
-                if ($resultado == NAN) {
-                    $resultado = 'Erro: Raiz inválida!';
-                }
                 break;
             default:
                 $resultado = 'Erro na seleção da operação!';
