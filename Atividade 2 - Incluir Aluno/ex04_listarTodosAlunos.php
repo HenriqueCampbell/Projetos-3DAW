c0d1321da1f14359246c8e9f643d87d1d1d57517
Henrique Campbell <hctrechau@gmail.com>
2026-03-16 19:54:03 -0300

    update listar alunos
    Agora faz conexão com editarAlunos.php

--- Atividade 2 - Incluir Aluno/ex04_listarTodosAlunos.php
+++ Atividade 2 - Incluir Aluno/ex04_listarTodosAlunos.php
@@ -5,6 +5,7 @@
 <body>
 <h1>Listar Alunos</h1>
 <h3>
+    <!-- Formulário de busca -->
     <form action="ex04_listarTodosAlunos.php" method="POST">
     Filtrar por Matrícula: <input type="text" name="busca_matricula">
     <input type="submit" value="Filtrar">
@@ -13,28 +14,40 @@
 </h3>
 
 <table>
-    <tr><th>Matricula</th><th>Nome</th><th>Email</th></tr>
+    <tr><th>Matricula</th><th>Nome</th><th>Email</th><th>Ação</th></tr>
 <?php
 
+    // Verifica se o formulário de busca foi submetido e captura o valor da matrícula.
     $busca = "";
-    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["busca_matricula"])) // Verifica se alguma busca ou filtro foi enviado.
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["busca_matricula"])) 
     {
         $busca = $_POST["busca_matricula"];
     }
 
    $arqAluno = fopen("alunos.txt","r") or die("Erro ao abrir o arquivo");
- 
+
+    $primeiraLinha = true;
+
    while(!feof($arqAluno)) {
+    
         $linha = fgets($arqAluno);
+
+        if ($primeiraLinha) {
+        $primeiraLinha = false;
+        continue;
+        }
+
         if (trim($linha) != "") 
         {
             $colunaDados = explode(";", $linha);
 
             if ($busca == "" || $colunaDados[0] == $busca)
-            { // Verifica se a busca estiver vazia ou se a matrícula está igual à busca, e exibe:
+            { 
                 echo "<tr><td>" . $colunaDados[0] . "</td>" .
                      "<td>" . $colunaDados[1] . "</td>" .
-                     "<td>" . $colunaDados[2] . "</td></tr>";
+                     "<td>" . $colunaDados[2] . "</td>" .
+                     // Gera o link para editar o aluno, passando a matrícula como parâmetro.
+                     "<td><a href='editarAluno.php?matricula=" . $colunaDados[0] . "'>Editar</a></td></tr>";
             }
         }
     }
