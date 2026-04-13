22083d3e3bb810a768c0d52d9ddd60f543fee591
Henrique Campbell <hctrechau@gmail.com>
2026-04-13 10:04:48 -0300

    update links para criação e listagem de perguntas

--- ProvaAV1/index.php
+++ ProvaAV1/index.php
@@ -9,10 +9,16 @@
             <a href="Cadastro.php">Cadastre um usuário</a>
         </p>
         <p>
-            <a href="Login.php">Criar uma pergunta Objetiva</a>
+            <a href="criarPerguntasObj.php">Criar uma pergunta Objetiva</a>
         </p>
         <p>
-            <a href="Login.php">Criar uma pergunta Subjetiva</a>
+            <a href="criarPerguntasSub.php">Criar uma pergunta Subjetiva</a>
+        </p>
+        <p>
+            <a href="listarPerguntasObj.php">Listar as perguntas Objetivas</a>
+        </p>
+        <p>
+            <a href="listarPerguntasSub.php">Listar as perguntas Subjetivas</a>
         </p>
     </body>
 </html>
