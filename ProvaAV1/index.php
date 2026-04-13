3046c9d7e60fa783369c8c615cb2a1bb9d1b9f5b
Henrique Campbell <hctrechau@gmail.com>
2026-04-13 09:47:11 -0300

    Adicionado index.php com HTML inicial.

---
+++ ProvaAV1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <h1> Selecione o que você deseja fazer: <h1>
+        <br><br> 
+        <p>
+            <a href="Cadastro.php">Cadastre um usuário</a>
+        </p>
+        <p>
+            <a href="Login.php">Criar uma pergunta Objetiva</a>
+        </p>
+        <p>
+            <a href="Login.php">Criar uma pergunta Subjetiva</a>
+        </p>
+    </body>
+</html>
