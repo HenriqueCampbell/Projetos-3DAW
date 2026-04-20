91ce3ab1ddbcb93cd71b451128d2d87a45c5ed97
Henrique Campbell <hctrechau@gmail.com>
2026-04-20 17:13:17 -0300

    Testando e Debugando Listagem Obj

--- ProvaAV1/listarPerguntasObj.php
+++ ProvaAV1/listarPerguntasObj.php
@@ -6,7 +6,7 @@
         <h1> Perguntas Objetivas Cadastradas </h1>
         <br>
         <table>
-            <tr><th>ID</th><th>Pergunta</th><th>Alternativa Correta</th><th>Alternativa Incorreta 1</th>th>Alternativa Incorreta 2</th>th>Alternativa Incorreta 3</th><th>Edição</th><th>Exclusão</th></tr>
+            <tr><th>ID</th><th>Pergunta</th><th>Alternativa Correta</th><th>Alternativa Incorreta 1</th><th>Alternativa Incorreta 2</th><th>Alternativa Incorreta 3</th><th>Edição</th><th>Exclusão</th></tr>
             <?php
                 $arqPerguntasObj = fopen("perguntasObj.txt", "r") or die("Erro na leitura do arquivo"); 
                 $primeiraLinha = true; 
@@ -14,22 +14,22 @@
                 while(!feof($arqPerguntasObj)) { 
                     $linha = fgets($arqPerguntasObj);
 
-                    if ($primeiraLinha = true) {
-                        $primeiraLinha = false
+                    if ($primeiraLinha) {
+                        $primeiraLinha = false;
                         continue; 
                     }
 
                     if (trim($linha) != "") { 
                         $coluna = explode(";", $linha); 
-                        echo "<tr><td>" . $coluna[0] . "</td><td>" . $coluna[1] . "</td><td>" . $coluna[2] . "</td><td>". $coluna[3] . "</td><td>" . $coluna[4] . "</td><td>" $coluna[5] . 
-                        "</td><td> . <a href='editarPerguntasObj.php?id=" . $coluna[0] . "'>Editar</a></td>" .
-                        "<td> . <a href='excluirPerguntasObj.php'?id=" . $coluna[0] . "'>Excluir</a></td></tr>";
+                        echo "<tr><td>" . $coluna[0] . "</td><td>" . $coluna[1] . "</td><td>" . $coluna[2] . "</td><td>".   $coluna[3] . "</td><td>" . $coluna[4] . "</td><td>" . $coluna[5] . 
+                        "</td><td>  <a href='editarPerguntasObj.php?id=" . $coluna[0] . "'>Editar</a></td>" .
+                        "<td>  <a href='excluirPerguntasObj.php'?id=" . $coluna[0] . "'>Excluir</a></td></tr>";
                     }
 
                 }
 
                 fclose($arqPerguntasObj);
-                echo "Leitura realizada com sucesso."
+                echo "Leitura realizada com sucesso.";
             ?>
         </table>
         <p>
